500d5cdfdde6d41e8582f6b1d3c640689d660176
Shashank Jain <shashank.jain97@gmail.com>
2018-10-31 12:16:20 -0500

    Updates

---
+++ precache-manifest.c1a1f858b3a19421dd42d06313eb2f45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b830451d91a1961fe5b5",
+    "url": "/tp/static/js/runtime~main.b830451d.js"
+  },
+  {
+    "revision": "5ab06474addfda1622d5",
+    "url": "/tp/static/js/main.5ab06474.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/tp/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "5ab06474addfda1622d5",
+    "url": "/tp/static/css/main.7b2762dd.chunk.css"
+  },
+  {
+    "revision": "022b7e0280c31e04921b3d81a00cb460",
+    "url": "/tp/index.html"
+  }
+];
